76693d7f7b82ab8add81520ecad6a64c09e317d7
Filip Hajduk <filip.hajduk@contactsales.pl>
2025-11-13 10:33:54 +0100

    d

--- my_font/core.py
+++ my_font/core.py
@@ -20,21 +20,12 @@ class myfont(IconPackMixin, InvenTreePlugin):
     DESCRIPTION = "A short d"
     VERSION = PLUGIN_VERSION
 
-    # Additional project information
     AUTHOR = "Filip Hajduk"
     WEBSITE = "https://github.com/Mistrzu4/my-font"
     LICENSE = "MIT"
 
-    # Optionally specify supported InvenTree versions
-    # MIN_VERSION = '0.18.0'
-    # MAX_VERSION = '2.0.0'
-
-    # Render custom UI elements to the plugin settings page
     ADMIN_SOURCE = "Settings.js:renderPluginSettings"
     
-    
-    # Plugin settings (from SettingsMixin)
-    # Ref: https://docs.inventree.org/en/latest/plugins/mixins/settings/
     SETTINGS = {
         # Define your plugin settings here...
         'CUSTOM_VALUE': {
@@ -45,24 +36,15 @@ class myfont(IconPackMixin, InvenTreePlugin):
         }
     }
     
-    
-    
-    
-    
-    
-
-    # User interface elements (from UserInterfaceMixin)
-    # Ref: https://docs.inventree.org/en/latest/plugins/mixins/ui/
-    
-    # Custom UI panels
-        
     def icon_packs(self):
         """Return a list of custom icon packs."""
         return [
             IconPack(
                 name='My Custom Icons',
                 prefix='my',
-                # fonts={}  <- pominięte całkowicie
+                fonts={
+                    'truetype': static('plugins/my-custom-plugin/icons/boxicons.ttf'),
+                },
                 icons={
                     'my-icon': {
                         'name': 'My Icon',
