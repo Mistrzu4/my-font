5d82669c81aa49e974507b5f328515d5c4e57932
Mistrzu4 <fajlip4@wp.pl>
2025-11-15 22:29:00 +0100

    test

--- my_font/core.py
+++ my_font/core.py
@@ -1,33 +1,23 @@
 from django.templatetags.static import static
-
+from django.http import JsonResponse
+from django.urls import path
 from common.icons import IconPack
 from plugin.base.icons.mixins import IconPackMixin
 from plugin.plugin import InvenTreePlugin
 
-
 PLUGIN_VERSION = "0.0.1"
 
-
-
 class myfont(IconPackMixin, InvenTreePlugin):
-
-    """myfont - custom InvenTree plugin."""
-
-    # Plugin metadata
     TITLE = "my font"
     NAME = "myfont"
     SLUG = "my-font"
-    DESCRIPTION = "A short d"
     VERSION = PLUGIN_VERSION
 
     AUTHOR = "Filip Hajduk"
     WEBSITE = "https://github.com/Mistrzu4/my-font"
     LICENSE = "MIT"
 
-    ADMIN_SOURCE = "Settings.js:renderPluginSettings"
-    
     SETTINGS = {
-        # Define your plugin settings here...
         'CUSTOM_VALUE': {
             'name': 'Custom Value',
             'description': 'A custom value',
@@ -36,32 +26,29 @@ class myfont(IconPackMixin, InvenTreePlugin):
         }
     }
 
-    def debug_font_url(request):
-        font_url = static('plugins/myfont/icons/boxicons.ttf')
-        return JsonResponse({"font_url": font_url})
+    def register_urls(self):
+        def debug_font_url(request):
+            font_url = static('plugins/myfont/icons/boxicons.ttf')
+            return JsonResponse({"font_url": font_url})
 
-    return [
-        path("debug-font/", debug_font_url),
-    ]
-
+        return [
+            path("debug-font/", debug_font_url),
+        ]
 
     def icon_packs(self):
-        """Return a list of custom icon packs."""
         return [
             IconPack(
                 name='My Custom Icons',
                 prefix='my',
                 fonts={
-                    'truetype': static('plugins/my-custom-plugin/icons/boxicons.ttf'),
+                    'truetype': static('plugins/myfont/icons/boxicons.ttf'),
                 },
                 icons={
                     'my-icon': {
                         'name': 'My Icon',
                         'category': '',
                         'tags': ['my', 'icon'],
-                        'variants': {
-                            'default': 'bx-message-circle-heart'  # tylko nazwa ikony z fontu
-                        }
+                        'variants': {'default': 'bx-message-circle-heart'}
                     }
                 },
             )
