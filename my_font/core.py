40c2dbda95282add7501b9b75f4a72ba61ae3560
Mistrzu4 <fajlip4@wp.pl>
2025-11-15 22:40:30 +0100

    test

--- my_font/core.py
+++ my_font/core.py
@@ -1,55 +1,17 @@
-from django.templatetags.static import static
-from django.http import JsonResponse
-from django.urls import path
-from common.icons import IconPack
-from plugin.base.icons.mixins import IconPackMixin
-from plugin.plugin import InvenTreePlugin
+def register_urls(self):
+    from django.urls import path
+    from django.templatetags.static import static
+    from django.http import JsonResponse
 
-PLUGIN_VERSION = "0.0.1"
+    def debug_font_url(request):
+        font_url = static('plugins/myfont/icons/boxicons.ttf')
+        # DEBUG: zapisz do pliku w kontenerze
+        try:
+            with open("/tmp/myfont_url.txt", "w") as f:
+                f.write(font_url)
+        except Exception as e:
+            with open("/tmp/myfont_url.txt", "w") as f:
+                f.write(f"ERROR: {e}")
+        return JsonResponse({"font_url": font_url})
 
-class myfont(IconPackMixin, InvenTreePlugin):
-    TITLE = "my font"
-    NAME = "myfont"
-    SLUG = "my-font"
-    VERSION = PLUGIN_VERSION
-
-    AUTHOR = "Filip Hajduk"
-    WEBSITE = "https://github.com/Mistrzu4/my-font"
-    LICENSE = "MIT"
-
-    SETTINGS = {
-        'CUSTOM_VALUE': {
-            'name': 'Custom Value',
-            'description': 'A custom value',
-            'validator': int,
-            'default': 42,
-        }
-    }
-
-    def register_urls(self):
-        def debug_font_url(request):
-            font_url = static('plugins/myfont/icons/boxicons.ttf')
-            return JsonResponse({"font_url": font_url})
-
-        return [
-            path("debug-font/", debug_font_url),
-        ]
-
-    def icon_packs(self):
-        return [
-            IconPack(
-                name='My Custom Icons',
-                prefix='my',
-                fonts={
-                    'truetype': static('plugins/myfont/icons/boxicons.ttf'),
-                },
-                icons={
-                    'my-icon': {
-                        'name': 'My Icon',
-                        'category': '',
-                        'tags': ['my', 'icon'],
-                        'variants': {'default': 'bx-message-circle-heart'}
-                    }
-                },
-            )
-        ]
+    return [path("debug-font/", debug_font_url)]
